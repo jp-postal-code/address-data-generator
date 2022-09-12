2e4f3d56f51e575aad8c0d2b10560ac8fef57527
shota <xyz.shotao@gmail.com>
2022-09-12 23:11:55 +0900

    test: アクションのテストを削除

--- src/__tests__/actions/minify-action.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@jest/globals';
-import { Command } from 'commander';
-import { minifyAction } from '@/lib/actions/minify-action';
-
-test('not implemented', async () => {
-  await expect(() =>
-    minifyAction('**/*', { pretty: true }, new Command())
-  ).rejects.toThrow('not implemented.');
-});
